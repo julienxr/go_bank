52f34f85cfc892db71936035ca3b6547fee0df43
Xavier <julienxr@gmail.com>
2024-09-25 20:21:53 -0500

    added config functionality

--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 
 	"github.com/julienxr/simplebank/api"
 	db "github.com/julienxr/simplebank/db/sqlc"
+	"github.com/julienxr/simplebank/utils"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err = server.Start(serverAddress); err != nil {
+	if err = server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
 
